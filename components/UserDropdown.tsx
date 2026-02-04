54368bd4f78ce3a7fdff844752423f527f8b7848
KarthikChilumula685 <karthikbunny2005@gmail.com>
2026-02-04 23:28:13 +0530

    Implement search Stocks

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -22,7 +22,7 @@ interface User {
   email: string;
 }
 
-const UserDropdown = ({ user }: { user: User }) => {
+const UserDropdown = ({ user, initialStocks }: { user: User, initialStocks: StockWithWatchlistStatus[] }) => {
   const router = useRouter();
 
   const handleSignOut = async () => {
@@ -97,7 +97,7 @@ const UserDropdown = ({ user }: { user: User }) => {
         <DropdownMenuSeparator className="hidden sm:block bg-gray-600" />
 
         <nav className="sm:hidden">
-          <NavItems />
+          <NavItems initialStocks={initialStocks} />
         </nav>
       </DropdownMenuContent>
     </DropdownMenu>
